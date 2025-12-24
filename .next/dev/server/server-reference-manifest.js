c06d45625ed3970adc5f1bc1464c85b1ab642f4b
Ullas Kunder <ullas@knidalhq.com>
2025-12-24 09:32:43 +0530

    remove .next from git cache

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"vZUhw0Ict/2YHxPHYQxBedo4ruvEu5TTdUsnzk+RZeE=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_e755559d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules__pnpm_ae83a536._.js",
-  "static/chunks/app_page_tsx_dda9881a._.js"
-],
-    source: "dynamic"
-});
